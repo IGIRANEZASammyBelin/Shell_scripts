e73ebfcc4609920a9519d9a27d9976058d353bb8
IGIRANEZASammyBelin <b.igiraneza1@alustudent.com>
2026-05-26 10:36:40 +0200

    files created

---
+++ name.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+Directory_Name="assignment_dir"
+Path="/c/Users/TULIP Group/shell_scripts"
+if [ -d "$Path" ];
+then
+echo "Directory with a Name '$Directory_Name'exits location"
+else
+echo "No such a Directoy"
+fi
+
